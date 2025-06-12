13b6daae80dad5059f959ddf93b28dee143b7dfc
elias hawkins <fouflord@gmail.com>
2025-06-11 22:11:00 -0700

    reworking resources

--- Scripts/rune_atlas.gd
+++ Scripts/rune_atlas.gd
@@ -1,21 +1,12 @@
 @tool
-extends Resource
-
-class_name runeItem
-
-@export var name: String = ""
-@export var speed: int
-@export var attack_range: int
-@export var attack_power: int
-@export var max_size: int
-@export var texture: Texture2D
-@export var tail_texture: Texture2D
-
+extends AtlasTexture
+class_name RuneTileTexture
 
 @export var atlas_texture: Texture2D = preload("res://assets/tiles/tile_hd.png") : set = set_atlas_texture
 @export var tile_index: Vector2i = Vector2i(0, 0) : set = set_tile_index
 @export var tile_size: int = 100 : set = set_tile_size
 
+@export var texture: Texture2D = null
 
 var _tile_index := Vector2i(0, 0)
 var _tile_size := 100
@@ -33,27 +24,18 @@ func set_tile_size(value):
 	tile_size = value
 	_update_preview()
 
-func set_tail_texture():
-	var region = Rect2((tile_index.x+1) * tile_size, tile_index.y * tile_size, tile_size, tile_size)
-	var tex := AtlasTexture.new()
-	tex.atlas = atlas_texture
-	tex.region = region
-	tex.filter_clip = true
-	tail_texture = tex
 # Needed for getters so inspector knows how to render values properly
 func get_tile_index(): return _tile_index
 func get_tile_size(): return _tile_size
 func get_atlas_texture(): return _atlas_texture
 
 func _update_preview():
+	print(tile_index)
 	
 	var region = Rect2(tile_index.x * tile_size, tile_index.y * tile_size, tile_size, tile_size)
 	var tex := AtlasTexture.new()
-	
 	tex.atlas = atlas_texture
 	tex.region = region
 	tex.filter_clip = true
-	
 	texture = tex
-	set_tail_texture()
-
+	print(tex)
